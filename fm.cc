0bbe148b03b8c4588d51f7f36a278ea26b441889
Stigl <stigl@ireview.games>
2025-05-29 22:48:00 +0200

    Added fm decode

--- fm.cc
+++ fm.cc
@@ -13,8 +13,12 @@ int FM::Encode(const Signal &in, Signal &out, FMOptions opt) {
 }
 
 int FM::Decode(const Signal &in, Signal &out, FMOptions opt) {
-    if(out.size() < in.size()) out.resize(in.size());
-    
+    size_t size = in.size();
+    if(out.size() < size) out.resize(in.size());
+    if(opt.Sensitivity > opt.CarrierFreq) return -1;
+
+    for(int t =0; t < size; t++)
+        out[t] = (sinhf(in[t] / opt.Amp) / 2 / M_PI / t) / opt.Sensitivity - opt.CarrierFreq;
     
     return 0;
 }
